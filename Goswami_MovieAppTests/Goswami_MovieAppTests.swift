8dedc5b76b00fbc4a65ad0828c5a44a1fab4e583
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-11-11 19:04:57 -0600

    Add initial unit test file for MovieApp
    Introduces Goswami_MovieAppTests.swift with a basic test structure and placeholder for future test cases.

---
+++ Goswami_MovieAppTests/Goswami_MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Goswami_MovieAppTests.swift
+//  Goswami_MovieAppTests
+//
+//  Created by Bhavesh on 11/5/25.
+//
+
+import Testing
+@testable import Goswami_MovieApp
+
+struct Goswami_MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
